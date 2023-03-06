97accca4b66a28aedde0658a4cc8f12e415196f4
Hendrik Mans <hendrik@mans.de>
2023-03-06 10:33:07 +0100

    Split things up

--- addons/rigidbody_auto_scaler/RigidBodyAutoScaler.gd
+++ addons/rigidbody_auto_scaler/RigidBodyAutoScaler.gd
@@ -10,17 +10,33 @@ func _exit_tree() -> void:
 
 
 func _perform(node: Node):
-	if node is RigidBody2D or node is RigidBody3D:
-		# If the node has a scale of 1, there's nothing for us to do.
-		if node.scale.x == 1: return
+	if node is RigidBody2D:
+		_scale_2d(node)
+	elif node is RigidBody3D:
+		_scale_3d(node)
 
-		# Apply scale to all children before we reset it
-		for child in node.get_children():
-			if child is Node3D or child is Node2D:
-				child.scale *= node.scale
 
-		# Reset the rigidbody's scale to 1
-		if node is RigidBody3D:
-			node.scale = Vector3.ONE
-		else:
-			node.scale = Vector2.ONE
+func _scale_2d(body: RigidBody2D):
+	# If the node has a scale of 1, there's nothing for us to do.
+	if body.global_scale.x == 1: return
+
+	# Apply scale to all children before we reset it
+	for child in body.get_children():
+		if child is Node2D:
+			child.scale *= body.global_scale
+
+	# Reset the rigidbody's scale to 1
+	body.global_scale = Vector2.ONE
+
+
+func _scale_3d(body: RigidBody3D):
+	# If the node has a scale of 1, there's nothing for us to do.
+	if body.scale.x == 1: return
+
+	# Apply scale to all children before we reset it
+	for child in body.get_children():
+		if child is Node3D or child is Node2D:
+			child.scale *= body.scale
+
+	# Reset the rigidbody's scale to 1
+	body.scale = Vector3.ONE
