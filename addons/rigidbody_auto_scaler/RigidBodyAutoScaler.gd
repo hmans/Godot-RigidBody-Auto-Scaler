ecdaad7844879571b3766acb511ecf9c9f3159d5
Hendrik Mans <hendrik@mans.de>
2023-03-06 21:12:04 +0100

    Comments

--- addons/rigidbody_auto_scaler/RigidBodyAutoScaler.gd
+++ addons/rigidbody_auto_scaler/RigidBodyAutoScaler.gd
@@ -30,13 +30,20 @@ func _scale_2d(body: RigidBody2D):
 
 
 func _scale_3d(body: RigidBody3D):
-	# If the node has a scale of 1, there's nothing for us to do.
+	# First, we'll set this node to use "top level" mode. This basically
+	# disconnects it from its parents transform3d (which a rigidbody3d would
+	# do either way, because it's simulated in the physics world). It has the
+	# added advantage of transferring the body's current world scale into
+	# its own transform, so we can access it through body.scale (there is
+	# no easy way to quickly get a node3d's world scale from a parented transform.)
 	body.top_level = true
+
+	# If the node has a scale of 1, there's nothing for us to do.
 	if body.scale.x == 1: return
 
 	# Apply scale to all children before we reset it
 	for child in body.get_children():
-		if child is Node3D or child is Node2D:
+		if child is Node3D:
 			child.scale *= body.scale
 
 	# Reset the rigidbody's scale to 1
